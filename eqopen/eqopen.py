98e1072dde8773be70f94675b70b9cdbae18f035
Ivan Radigales Creus <ivanrdgc@gmail.com>
2022-12-28 17:56:36 +0100

    Return library instance

--- eqopen/eqopen.py
+++ eqopen/eqopen.py
@@ -1,6 +1,6 @@
 import google.colab, gspread, google.auth, os, IPython.display
 
-def eq_backtests(eq_credentials_path: str) -> None:
+def eq_backtests(eq_credentials_path: str = 'EQCredentials', *argc, **argv) -> 'EQBacktests':
     # Read EQCredentials
     google.colab.auth.authenticate_user()
     gspread.gc = gspread.authorize(google.auth.default()[0])
@@ -15,4 +15,6 @@ def eq_backtests(eq_credentials_path: str) -> None:
     # Clean pip install output
     IPython.display.clear_output()
 
-    return eq_credentials
+    # Import and return library
+    import eq_backtests
+    return eq_backtests.EQBacktests(eq_credentials, *argc, **argv)
